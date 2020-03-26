45bdcebf28655af6fa17d270ad7b033c1505ea72
Jen Kagan <jen@netlify.com>
2020-03-26 15:08:07 -0700

    Add form with redirect to custom success page

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <h1>{{ teaName }}</h1>
+      <h2>Pick a Tea</h2>
     </div>
     <div>
       <form
+        netlify
         name="vue-tea"
         method="post"
-        data-netlify="true"
         data-netlify-honeypot="bot-field"
+        @submit.prevent="handleSubmit"
       >
         <input type="hidden" name="form-name" value="vue-tea" />
         <div class="tea">
-          <h2>Pick a Tea</h2>
           <label
             v-for="tea in teaNames"
             :key="tea"
@@ -22,7 +22,6 @@
             }"
           >
             <input
-              id="tea"
               v-model="chosenTea"
               name="tea"
               type="radio"
@@ -31,6 +30,9 @@
             <span>{{ tea }}</span>
           </label>
         </div>
+        <div>
+          <h1>{{ teaName }}</h1>
+        </div>
         <button>Submit</button>
       </form>
     </div>
@@ -38,6 +40,8 @@
 </template>
 
 <script>
+// Many thanks: https://www.netlify.com/blog/2018/09/07/how-to-integrate-netlify-forms-in-a-vue-app/
+
 export default {
   data() {
     return {
@@ -61,6 +65,34 @@ export default {
     teaNames() {
       return Object.keys(this.teaTypes)
     }
+  },
+  methods: {
+    // This function puts all the form fields into a FormData constructor, which we later encode with the URLSearchParams constructor
+    createFormDataObj(data) {
+      const formData = new FormData();
+      for (const key of Object.keys(data)) {
+        formData.append(key, data[key]);
+      }
+      return formData;
+    },
+    // This is our custom onSubmit function; don't forget to add `@submit.prevent="handleSubmit"` inside your <form> tag
+    handleSubmit () {
+      // This `data` object is what's passed to the createFormDataObj function. It needs all of your form fields, where the key is the name= attribute and the value is the computed value.
+      const data = {
+        "form-name": "vue-tea",
+        "tea": this.teaName
+      }
+      // This POSTs your encoded form to Netlify with the required headers (for text; headers will be different for POSTing a file) and, on success, redirects to the custom success page located at pages/thanks.vue
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(this.createFormDataObj(data)).toString()
+      })
+      // This is how we route to /thanks on successful form submission
+      // More on $router.push function: https://router.vuejs.org/guide/essentials/navigation.html
+      .then(() => this.$router.push('thanks'))
+      .catch(error => alert(error))
+    }
   }
 }
 </script>
@@ -73,27 +105,13 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+.tea {
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+  padding: 1.5em;
 }
 </style>
---
+++ pages/thanks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container">
+    <div>
+      <h1>Thanks for submitting your tea!</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
